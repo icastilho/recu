9f86643c5b4415a61c2a9bf5381b46569437ccc7
Diego Domingues <diego.domingues16@gmail.com>
2014-07-11 14:26:54 -0300

    Implementando login usando UserApi

--- assets/js/app.js
+++ assets/js/app.js
@@ -69,3 +69,14 @@
   window.io
 
 );
+
+var module = angular.module('starter', ['UserApp', 'starter.controllers'])
+  .run(function(user) {
+    user.init({ appId: '53bfe9bd1016c' });
+  })
+  .config(['$routeProvider',function($routeProvider) {
+    $routeProvider
+       .when('/login', {templateUrl: 'home/login.ejs', login: true})
+       .when('/signup', {templateUrl: 'home/signup.html', public: true})
+       .otherwise('/index.ejs');
+  }]);
