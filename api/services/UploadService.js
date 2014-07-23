a287baffee20a8c9a050c3ec5dd4086dc16cf585
André Midea <midea.andre@gmail.com>
2014-07-23 08:29:08 -0300

    Fazendo uplaod

--- api/services/UploadService.js
+++ api/services/UploadService.js
@@ -1,8 +1,10 @@
-var fs = require('fs');
-var unzip = require('unzip');
-var xml2js = require('xml2js');
-var Q = require('q');
-var S = require('string');
+var fs = require('fs'),
+    unzip = require('unzip'),
+    xml2js = require('xml2js'),
+    Q = require('q'),
+    S = require('string'),
+    path = require('path'),
+    temp = require('temp');
 
 
 
@@ -14,35 +16,50 @@ UploadService.prototype.upload = function(upload, callback){
     fs.createReadStream(upload.path)
         .pipe(unzip.Parse())
         .on('entry', function (arquivo) {
-            if(S(arquivo.path).contains('MACOSX')) return;
+            if(invalidarArquivo(arquivo.path)) return;
 
             Q.fcall(parsearArquivo, arquivo)
              .then(validar);
         })
         .on('close', callback);
 }
+
 function parsearArquivo(arquivo){
     var deferred = Q.defer();
     var parser = new xml2js.Parser();
 
-    arquivo.pipe(fs.createWriteStream(arquivo.path).on('close', function(){
-        fs.readFile(arquivo.path, function (error, data) {
+    temp.track();
 
-            parser.parseString(data, function (err, result) {
-                if (error) {
-                    deferred.reject(new Error(error));
-                } else{
-                    deferred.resolve(result);
-                }
+    temp.mkdir('temp', function(err, dirPath) {
+        var inputPath = path.join(dirPath, arquivo.path);
+
+        arquivo.pipe(fs.createWriteStream(inputPath).on('close', function(){
+            fs.readFile(inputPath, function (error, data) {
+
+                parser.parseString(data, function (err, result) {
+                    if (error) {
+                        deferred.reject(new Error(error));
+                    } else{
+                        deferred.resolve(result);
+                    }
+                });
             });
-        });
-    }));
+        }))
+    });;
 
     return deferred.promise;
 }
 
-function validar(notaJson) {
-    console.log(notaJson);
+function invalidarArquivo(path){    
+    return S(path).contains('MACOSX') || !(S(path).endsWith('.xml'))
+}
+
+function validar(err, notaJson) {    
+    if(err){
+        console.log("error :"+err);
+    } else {
+        console.log(notaJson);
+    }    
 }
 
 module.exports = UploadService;
