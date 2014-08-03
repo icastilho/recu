9dc51596bdc6c34a842eb258b41f28f1ee240311
icastilho <italorfc@gmail.com>
2014-08-02 22:21:58 -0300

    Correcao Consulta Selic

--- api/controllers/SelicController.js
+++ api/controllers/SelicController.js
@@ -1,16 +1,18 @@
 var SelicService = require('../services/SelicService.js')
 var moment = require('moment');
+var BigNumber = require('bignumber.js');
 var service = new SelicService();
-
+moment.lang('pt');
 var SelicController = {
-	
-	consultar: function(req, res){
-        var data = moment(req.param('data'), "DD/MM/YYYY").format(), valor = req.param('valor');
 
+	consultar: function(req, res){
+        var data = moment(req.param('data'), "DD/MM/YYYY").format();
+        data = new Date(data);
+        var valor = BigNumber(req.param('valor'));
         service.consultar(data, valor, function(valorAtualizado){
             res.send({
                 dataReferencia: moment().format('l'),
-                valor: valorAtualizado
+                valor: valorAtualizado.round(2).toString()
             });
         })
 	},
--- api/services/SelicService.js
+++ api/services/SelicService.js
@@ -2,7 +2,6 @@ var request = require('request');
 var math = require('mathjs');
 var split = require('split');
 var moment = require('moment');
-var BigNumber = require('bignumber.js');
 
 function SelicService() {
 
@@ -11,8 +10,7 @@ function SelicService() {
 SelicService.prototype.consultar = function (data, valor, callback) {
     Selic.find()
         .where({ data: { '>=': data}})
-        .exec(function(err, selics) {
-            console.log("selics "+selics)
+            .exec(function(err, selics) {
             _.each(selics, function(selic, i) {
                 if(selic.fatorDiario > 0){
                     valor = valor.times(selic.fatorDiario)
