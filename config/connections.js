0c8d37c26bb24a1a916010eda8a2527636c50bd8
Diego Henrique Domingues <diego.domingues16@gmail.com>
2014-08-06 00:14:32 -0300

    Ajuste de conexões

--- config/connections.js
+++ config/connections.js
@@ -1,16 +1,16 @@
 /**
  * Connections
- * 
+ *
  * `Connections` are like "saved settings" for your adapters.  What's the difference between
  * a connection and an adapter, you might ask?  An adapter (e.g. `sails-mysql`) is generic--
- * it needs some additional information to work (e.g. your database host, password, user, etc.) 
+ * it needs some additional information to work (e.g. your database host, password, user, etc.)
  * A `connection` is that additional information.
- * 
+ *
  * Each model must have a `connection` property (a string) which is references the name of one
  * of these connections.  If it doesn't, the default `connection` configured in `config/models.js`
  * will be applied.  Of course, a connection can (and usually is) shared by multiple models.
  * .
- * Note: If you're using version control, you should put your passwords/api keys 
+ * Note: If you're using version control, you should put your passwords/api keys
  * in `config/local.js`, environment variables, or use another strategy.
  * (this is to prevent you inadvertently sensitive credentials up to your repository.)
  *
@@ -20,26 +20,24 @@
 
 module.exports.connections = {
 
-  localDiskDb: {
-    adapter: 'sails-disk'
-  },
+   localDiskDb: {
+      adapter: 'sails-disk'
+   },
 
-  mongo: {
-    adapter   : 'sails-mongo',
-     host: 'kahana.mongohq.com',
-     port: 10033,
-     user: 'admin',
-     password: 'admin',
-     database: 'app27533068'
-  }
-//    mongo: {
-//        module: 'sails-mongo',
-//        host: 'localhost',
-//        port: 27017,
-//        user: '',
-//        password: '',
-//        database: 'recu'
-//    }
+   localMongo: {
+      adapter: 'sails-mongo',
+      host: 'localhost',
+      port: 27017
+   },
+
+   mongo: {
+      adapter: 'sails-mongo',
+      host: 'kahana.mongohq.com',
+      port: 10033,
+      user: 'admin',
+      password: 'admin',
+      database: 'app27533068'
+   }
 
 };
 
