bd024aaf9f068a2d715e567e13c43c053e3dfd1e
André Midea <midea.andre@gmail.com>
2014-08-05 23:02:20 -0300

    Correcoes

--- config/log.js
+++ config/log.js
@@ -2,25 +2,7 @@ var scribe = require('scribe');
 
 // Configuration
 // --------------
-scribe.configure(function(){
-    scribe.set('app', 'GFI');                     // NOTE Best way learn about these settings is
-    scribe.set('logPath', './log'); // Doublecheck       // them out for yourself.
-    scribe.set('defaultTag', 'DEFAULT');
-    scribe.set('divider', ':::');
-    scribe.set('identation', 5);                          // Identation before console messages
 
-    scribe.set('maxTagLength', 30);                       // Any tags that have a length greather than
-    // 30 characters will be ignored
-});
-
-// Create Loggers
-// --------------
-scribe.addLogger("log", false , true, 'white');            // (name, save to file, print to console,
-scribe.addLogger('realtime', true, true, 'underline');    // tag color)
-scribe.addLogger('high', true, true, 'magenta');
-scribe.addLogger('normal', true, true, 'white');
-scribe.addLogger('low', true, true, 'grey');
-scribe.addLogger('info', true, true, 'cyan');
 
 
 
