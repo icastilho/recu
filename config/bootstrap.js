feb2bd94afd631b4e9d56ae9855768fe5385a775
Diego Domingues <diego.domingues16@gmail.com>
2014-08-13 19:24:44 -0300

    Atualizando para sails 0.10.4

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -1,11 +1,12 @@
 /**
  * Bootstrap
+ * (sails.config.bootstrap)
  *
  * An asynchronous bootstrap function that runs before your Sails app gets lifted.
  * This gives you an opportunity to set up your data model, run jobs, or perform some special logic.
  *
  * For more information on bootstrapping your app, check out:
- * http://sailsjs.org/#documentation
+ * http://sailsjs.org/#/documentation/reference/sails.config/sails.config.bootstrap.html
  */
 
 var UploadJob = require("../api/services/UploadJob.js");
@@ -18,18 +19,18 @@ var colors = require('colors');
 
 module.exports.bootstrap = function (cb) {
 
-    moment.lang('pt');
-    var upload = new UploadJob();
+   moment.lang('pt');
+   var upload = new UploadJob();
 
 
-    new CronJob('0 0 5 * * *', function(){
-        console.info("Atualizando Selic... ".green);
+   new CronJob('0 0 5 * * *', function () {
+      console.info("Atualizando Selic... ".green);
 
-        service.atualizarSelic();
-    }, null, true, "America/Sao_Paulo");
+      service.atualizarSelic();
+   }, null, true, "America/Sao_Paulo");
 
 
-  // It's very important to trigger this callack method when you are finished 
-  // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
-  cb();
+   // It's very important to trigger this callback method when you are finished
+   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
+   cb();
 };
--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -15,19 +15,21 @@
 // (if you're using LESS with the built-in default config, you'll want
 //  to change `assets/styles/importer.less` instead.)
 var cssFilesToInject = [
+//   'styles/**/*.css'
+
    <!-- Basic Styles -->
-   'linker/css/bootstrap.min.css',
-   'linker/css/font-awesome.min.css',
+   'css/bootstrap.min.css',
+   'css/font-awesome.min.css',
 
    <!-- SmartAdmin Styles : Please note (smartadmin-production.css) was created using LESS variables -->
-   'linker/css/smartadmin-production.min.css',
-   'linker/css/smartadmin-skins.min.css',
+   'css/smartadmin-production.min.css',
+   'css/smartadmin-skins.min.css',
 
    <!-- SmartAdmin RTL Support is under construction-->
-   'linker/css/smartadmin-rtl.min.css',
+   'css/smartadmin-rtl.min.css',
 
    <!-- Goes with demo.js, you can delete this css when designing your own WebApp -->
-   'linker/css/demo.min.css'
+   'css/demo.min.css'
 ];
 
 
@@ -35,82 +37,88 @@ var cssFilesToInject = [
 // (uses Grunt-style wildcard/glob/splat expressions)
 var jsFilesToInject = [
 
-   'linker/js/libs/jquery-2.0.2.min.js',
-   'linker/js/libs/jquery-ui-1.10.3.min.js',
-   'linker/js/app.config.js',
+   // Load sails.io before everything else
+   'js/dependencies/sails.io.js',
 
-   <!-- JS TOUCH : include this plugin for mobile drag / drop touch events-->
-   'linker/js/plugin/jquery-touch/jquery.ui.touch-punch.min.js',
+   'js/dependencies/jquery-2.0.2.min.js',
+   'js/dependencies/jquery-ui-1.10.3.min.js',
+   'js/dependencies/app.config.js',
 
    <!-- BOOTSTRAP JS -->
-   'linker/js/bootstrap/bootstrap.min.js',
-
-   <!-- CUSTOM NOTIFICATION -->
-   'linker/js/notification/SmartNotification.min.js',
-
-   <!-- JARVIS WIDGETS -->
-   'linker/js/smartwidgets/jarvis.widget.min.js',
-
-   <!-- EASY PIE CHARTS -->
-   'linker/js/plugin/easy-pie-chart/jquery.easy-pie-chart.min.js',
-
-   <!-- SPARKLINES -->
-   'linker/js/plugin/sparkline/jquery.sparkline.min.js',
-
-   <!-- JQUERY VALIDATE -->
-   'linker/js/plugin/jquery-validate/jquery.validate.min.js',
-
-   <!-- JQUERY MASKED INPUT -->
-   'linker/js/plugin/masked-input/jquery.maskedinput.min.js',
-
-   <!-- JQUERY SELECT2 INPUT -->
-   'linker/js/plugin/select2/select2.min.js',
-
-   <!-- JQUERY UI + Bootstrap Slider -->
-   'linker/js/plugin/bootstrap-slider/bootstrap-slider.min.js',
-
-   <!-- browser msie issue fix -->
-   'linker/js/plugin/msie-fix/jquery.mb.browser.min.js',
-
-   <!-- FastClick: For mobile devices: you can disable this in app.js -->
-   'linker/js/plugin/fastclick/fastclick.min.js',
+   'js/dependencies/bootstrap.min.js',
 
    <!-- AngularJS -->
-   'linker/js/libs/angular/angular.js',
-   'linker/js/libs/angular/angular-route.js',
-   'linker/js/libs/angular/angular-animate.js',
-   'linker/js/libs/angular/angular-ui-router.js',
-   'linker/js/libs/angular/angular-locale_pt-br.js',
-   'linker/js/angular-file-upload/angular-file-upload.min.js',
-   'linker/js/libs/angular/ui-bootstrap-custom-tpls-0.11.0.js',
+   'js/dependencies/angular/angular.js',
+   'js/dependencies/angular/angular-route.js',
+   'js/dependencies/angular/angular-animate.js',
+   'js/dependencies/angular/angular-ui-router.js',
+   'js/dependencies/angular/angular-locale_pt-br.js',
 
-    <!-- Upload -->
-    'linker/js/es5-shim/es5-shim.min.js',
-    'linker/js/es5-shim/es5-sham.min.js',
+   <!-- CUSTOM NOTIFICATION -->
+   'js/notification/SmartNotification.min.js',
 
-   <!-- Userapp.io -->
-   'linker/js/libs/userapp.io/userapp.client.js',
-   'linker/js/libs/userapp.io/angularjs.userapp.js',
+   'js/dependencies/userapp.io/userapp.client.js',
+   'js/dependencies/userapp.io/angularjs.userapp.js',
+
+   // Dependencies like jQuery, or Angular are brought in here
+
+//   'js/dependencies/**/*.js',
+   // All of the rest of your client-side js files
+   // will be injected here in no particular order.
+
+//   'js/**/*.js',
+
+   <!-- JS TOUCH : include this plugin for mobile drag / drop touch events-->
+   'js/plugin/jquery-touch/jquery.ui.touch-punch.min.js',
+
+   <!-- JARVIS WIDGETS -->
+   'js/smartwidgets/jarvis.widget.min.js',
+
+   <!-- EASY PIE CHARTS -->
+   'js/plugin/easy-pie-chart/jquery.easy-pie-chart.min.js',
+
+   <!-- SPARKLINES -->
+   'js/plugin/sparkline/jquery.sparkline.min.js',
+
+   <!-- JQUERY VALIDATE -->
+   'js/plugin/jquery-validate/jquery.validate.min.js',
+
+   <!-- JQUERY MASKED INPUT -->
+   'js/plugin/masked-input/jquery.maskedinput.min.js',
+
+   <!-- JQUERY SELECT2 INPUT -->
+   'js/plugin/select2/select2.min.js',
+
+   <!-- JQUERY UI + Bootstrap Slider -->
+   'js/plugin/bootstrap-slider/bootstrap-slider.min.js',
+
+   <!-- browser msie issue fix -->
+   'js/plugin/msie-fix/jquery.mb.browser.min.js',
+
+   <!-- FastClick: For mobile devices: you can disable this in app.js -->
+   'js/plugin/fastclick/fastclick.min.js',
+
+   <!-- AngularJS -->
+   'js/angular-file-upload/angular-file-upload.min.js',
+   'js/dependencies/angular/ui-bootstrap-custom-tpls-0.11.0.js',
+
+   <!-- Upload -->
+   'js/es5-shim/es5-shim.min.js',
+   'js/es5-shim/es5-sham.min.js',
 
    <!-- MAIN APP JS FILE -->
-   'linker/js/app.js',
+   'js/app.js',
 
-   'linker/js/controllers/dashboard.js',
-   'linker/js/controllers/fileupload.js',
-   'linker/js/controllers/lote.js',
-   'linker/js/controllers/selic.js',
-   'linker/js/controllers/apuracao.js',
+   'js/controllers/dashboard.js',
+   'js/controllers/fileupload.js',
+   'js/controllers/lote.js',
+   'js/controllers/selic.js',
+   'js/controllers/apuracao.js',
 
    <!-- MAIN ANGULAR JS FILE -->
-   'linker/js/ng/ng.app.js',
-   'linker/js/ng/ng.controllers.js',
-   'linker/js/ng/ng.directives.js',
-
-   // Bring in the socket.io client
-   'linker/js/socket.io.js',
-
-   // then beef it up with some convenience logic for talking to Sails.js
-   'linker/js/sails.io.js'
+   'js/ng/ng.app.js',
+   'js/ng/ng.controllers.js',
+   'js/ng/ng.directives.js'
 ];
 
 
@@ -124,13 +132,8 @@ var jsFilesToInject = [
 // templates get spit out to the same file.  Be sure and check out `tasks/README.md`
 // for information on customizing and installing new tasks.
 var templateFilesToInject = [
-  'templates/**/*.html'
+   'templates/**/*.html'
 ];
-
-
-
-
-
 
 
 
@@ -138,11 +141,11 @@ var templateFilesToInject = [
 // (i.e. where the other Grunt tasks spit them out, or in some cases, where
 // they reside in the first place)
 module.exports.cssFilesToInject = cssFilesToInject.map(function(path) {
-  return '.tmp/public/' + path;
+   return '.tmp/public/' + path;
 });
 module.exports.jsFilesToInject = jsFilesToInject.map(function(path) {
-  return '.tmp/public/' + path;
+   return '.tmp/public/' + path;
 });
 module.exports.templateFilesToInject = templateFilesToInject.map(function(path) {
-  return 'assets/' + path;
+   return 'assets/' + path;
 });
