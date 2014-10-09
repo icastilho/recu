cd0c73611f9c2d9a51766f9e660f162cf4a446e9
André Midea <midea.andre@gmail.com>
2014-10-09 18:02:29 -0300

    Otimizando selic!

--- api/services/SelicService.js
+++ api/services/SelicService.js
@@ -2,62 +2,75 @@ var request = require('request');
 var math = require('mathjs');
 var split = require('split');
 var moment = require('moment');
+var async = require('async');
+var BigNumber = require('bignumber.js');
+var HashMap = require('hashmap').HashMap;
+var selicAcumulado = new HashMap();
 
 function SelicService() {
-   this.consultar = function (data, valor, callback) {
-      Selic.find()
-         .where({ data: { '>=': data}})
-         .exec(function(err, selics) {
-            selics.forEach(function(selic) {
-               if (selic.fatorDiario > 0)
-                  valor = valor.times(selic.fatorDiario)
-            });
+  this.consultar = function (data, valor, callback) {
 
-            callback(valor.round(2));
-         });
-   }
+    if (selicAcumulado.count() == 0) {
+      Selic.find().sort('data desc').exec(function (err, selics) {
+        var acumulado = BigNumber(1);
 
-   this.atualizarSelic = function () {
-      var data = {
-         dataInicial: moment().subtract('years', 10).startOf('year').format('l'),
-         dataFinal: moment().format('l'),
-         method: 'listarTaxaDiaria',
-         tipoApresentacao: 'arquivo',
-         Submit: 'Consultar'
+        selics.forEach(function (selic) {
+          if (selic.fatorDiario > 0)
+            acumulado = acumulado.times(selic.fatorDiario);
+
+          selicAcumulado.set(selic.data, acumulado);
+        });
+
+        valor = valor.times(selicAcumulado.get(data));
+        callback(valor.round(2));
+      });
+    } else {
+      valor = valor.times(selicAcumulado.get(data));
+      callback(valor.round(2));
+    }
+  }
+
+  this.atualizarSelic = function () {
+    var data = {
+      dataInicial: moment().subtract('years', 10).startOf('year').format('l'),
+      dataFinal: moment().format('l'),
+      method: 'listarTaxaDiaria',
+      tipoApresentacao: 'arquivo',
+      Submit: 'Consultar'
+    };
+
+    console.log(data);
+
+    Selic.destroy({}).exec(function (err) {
+      if (err)
+        console.log(err);
+    });
+
+    request.post('http://www3.bcb.gov.br/selic/consulta/taxaSelic.do', {form: data}).pipe(split()).on('data', function (line) {
+      var split = line.split(';');
+
+      if (split[1] == undefined) {
+        return;
+      }
+
+      var taxa = parseFloat(split[1].replace(',', '.'));
+      var fatorDiario = parseFloat(split[2].replace(',', '.'));
+
+      var selic = {
+        data: moment(split[0], 'DD/MM/YYYY').format(),
+        taxa: math.round(taxa, 3),
+        fatorDiario: fatorDiario
       };
 
-      console.log(data);
-
-      Selic.destroy({}).exec(function(err){
-         if (err)
-            console.log(err);
+      Selic.create(selic).exec(function (err, user) {
+        if (err) {
+          return console.log(err);
+        } else {
+          console.log("Selic created:", user);
+        }
       });
-
-      request.post('http://www3.bcb.gov.br/selic/consulta/taxaSelic.do', {form: data}).pipe(split()).on('data', function (line) {
-         var split = line.split(';');
-
-         if(split[1] == undefined){
-            return;
-         }
-
-         var taxa = parseFloat(split[1].replace(',', '.'));
-         var fatorDiario = parseFloat(split[2].replace(',', '.'));
-
-         var selic = {
-            data: moment(split[0], 'DD/MM/YYYY').format(),
-            taxa: math.round(taxa, 3),
-            fatorDiario: fatorDiario
-         };
-
-         Selic.create(selic).exec(function (err, user) {
-            if (err) {
-               return console.log(err);
-            } else {
-               console.log("Selic created:", user);
-            }
-         });
-      });
-   }
+    });
+  }
 }
 
 module.exports = new SelicService;
